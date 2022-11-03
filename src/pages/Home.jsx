e12d8b31617e159b2c5912760e4c85eed871a3c0
lina <benahmed.lina@yahoo.fr>
2022-11-03 17:41:41 +0100

    3rd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import data from "data/data.json";
+////import React from "react";
+//import { Link } from "react-router-dom";
+//import data from "data/data.json";
 //import banner from "components/banner.jsx";
 
-const Appartments = () => {
-  return;
+//const Appartments = () => {
+// return;
 
-  <div className="home">
-    <div className="home_appartments">{data.map}</div>
-    <Link className="link" to="/">
-      Accueil
-    </Link>
-  </div>;
-};
+//<div className="home">
+//  <div className="home_appartments">{data.map}</div>
+// <Link className="link" to="/">
+//   Accueil
+//  </Link>
+// </div>;
+//};
 
-export default Appartments;
+//export default Appartments;
